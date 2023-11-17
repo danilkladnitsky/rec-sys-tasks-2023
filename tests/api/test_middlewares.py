7db8c69395b1138958420ce785c2c2d734d9e133
Danil Kladnitsky <danyakladnitsky@gmail.com>
2023-11-17 13:17:55 +0300

    feat: add tests

--- tests/api/test_middlewares.py
+++ tests/api/test_middlewares.py
@@ -5,11 +5,19 @@ from starlette.testclient import TestClient
 API_KEY_HEADER = "default_api_key"
 
 
+def test_public_url(
+    client: TestClient,
+) -> None:
+    with client:
+        response = client.get("/health")
+    assert response.status_code == HTTPStatus.OK
+
+
 def test_invalid_api_key(
     client: TestClient,
 ) -> None:
     with client:
-        response = client.get("/health", headers={"Api-Key": "validJWTiPromise"})
+        response = client.get("/reco/sample_model/1", headers={"Api-Key": "validJWTiPromise"})
     assert response.status_code == HTTPStatus.UNAUTHORIZED
 
 
@@ -17,5 +25,5 @@ def test_valid_api_key(
     client: TestClient,
 ) -> None:
     with client:
-        response = client.get("/health", headers={"Api-Key": API_KEY_HEADER})
+        response = client.get("/reco/sample_model/1", headers={"Api-Key": API_KEY_HEADER})
     assert response.status_code == HTTPStatus.OK
